b88c54eb3ea1896acace1212db8f909e2cedc331
Huynh Lam <52045257+lahuynt@users.noreply.github.com>
2020-08-13 20:38:40 +0700

    ✔ J1.S.P0001 - Bubble Sort

---
+++ J1.S.P0001 - Bubble Sort/src/bs/utils/Utils.java
@@ -0,0 +1,70 @@
+package bs.utils;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lamhnt
+ */
+public class Utils {
+
+    // declare
+    Scanner sc = new Scanner(System.in);
+    int arr[];
+
+    // check number
+    public int checkInsNum() {
+        while (true) {
+            try {
+                int checkedNum = Integer.parseInt(sc.nextLine().trim());
+                if (checkedNum < 0) {
+                    throw new NumberFormatException();
+                } else {
+                    return checkedNum;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a POSITIVE NUMBER again!");
+                System.out.print("Enter number of Array: ");
+            }
+        }
+    }
+
+    // insert number
+    public int insertNum() {
+        int insNum = 0;
+
+        System.out.print("Enter number of Array: ");
+        insNum = checkInsNum();
+
+        return insNum;
+    }
+
+    // random number into array
+    public int[] randNum(int insNum) {
+        arr = new int[insNum];
+        Random rand = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+
+        return arr;
+    }
+
+    // bubble sort
+    public void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
