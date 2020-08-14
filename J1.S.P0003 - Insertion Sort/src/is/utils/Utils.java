2d67d2d7d5c3b4891fe01480812783e087e4091e
Huynh Lam <52045257+lahuynt@users.noreply.github.com>
2020-08-14 11:46:55 +0700

    ✔ J1.S.P0003 - Insertion Sort

---
+++ J1.S.P0003 - Insertion Sort/src/is/utils/Utils.java
@@ -0,0 +1,72 @@
+package is.utils;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lamhnt
+ */
+public class Utils {
+
+    // declare
+    Scanner sc = new Scanner(System.in);
+    int arr[];
+
+    // check number
+    public int checkInsNum() {
+        while (true) {
+            try {
+                int checkedNum = Integer.parseInt(sc.nextLine().trim());
+                if (checkedNum < 0) {
+                    throw new NumberFormatException();
+                } else {
+                    return checkedNum;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a POSITIVE NUMBER again!");
+                System.out.print("Enter number of Array: ");
+            }
+        }
+    }
+
+    // insert number
+    public int insertNum() {
+        int insNum = 0;
+
+        System.out.print("Enter number of Array: ");
+        insNum = checkInsNum();
+
+        return insNum;
+    }
+
+    // random number into array
+    public int[] randNum(int insNum) {
+        arr = new int[insNum];
+        Random rand = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+
+        return arr;
+    }
+
+    // selection sort
+    public void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+
+            arr[j + 1] = key;
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
